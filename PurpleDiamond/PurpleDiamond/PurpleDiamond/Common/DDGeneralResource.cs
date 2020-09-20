e80f98151fdc7171a551682899a6c938eeaf848b
stackprobe <stackprobes@gmail.com>
2020-09-20 23:53:22 +0900

    Eirene

--- PurpleDiamond/PurpleDiamond/PurpleDiamond/Common/DDGeneralResource.cs
+++ PurpleDiamond/PurpleDiamond/PurpleDiamond/Common/DDGeneralResource.cs
@@ -8,8 +8,14 @@ namespace Charlotte.Common
 	//
 	//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
 	//
+	/// <summary>
+	/// <para>全プロジェクトで共通のリソース</para>
+	/// <para>当該リソースは Resource/General フォルダに収録すること。</para>
+	/// </summary>
 	public class DDGeneralResource
 	{
+		// game_app.ico --> DDMain.GetAppIcon()
+
 		//
 		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
 		//
@@ -22,6 +28,11 @@ namespace Charlotte.Common
 		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
 		//
 		public DDPicture WhiteCircle = DDPictureLoaders.Standard(@"General\WhiteCircle.png");
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public DDMusic 無音 = new DDMusic(@"General\muon.wav");
 
 		// 新しいリソースをここへ追加...
 	}
